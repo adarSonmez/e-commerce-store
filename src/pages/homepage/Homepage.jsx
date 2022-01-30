6f0967e0cf17756e6e3c96f95a725ee3ccdbac59
Adar Sönmez <adarsonmez@outlook.com>
2022-01-30 18:53:02 +0300

    Homepage designed

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Directory } from '../components/directory/Directory';
+import './Homepage.scss';
+
+export const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
