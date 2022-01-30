25f3f84e06f60731c20727e55f62ae3067376d57
Adar Sönmez <adarsonmez@outlook.com>
2022-01-30 18:58:40 +0300

    Homepage displays categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Homepage } from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      Hello React!
+      <Homepage />
     </div>
   );
 }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Directory } from '../components/directory/Directory';
+import { Directory } from '../../components/directory/Directory';
 import './Homepage.scss';
 
 export const Homepage = () => {
