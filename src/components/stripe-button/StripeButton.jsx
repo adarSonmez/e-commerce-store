9e43f4888f64aeeabf46b3eae1efaae0973c3fad
Adar Sönmez <adarsonmez@outlook.com>
2022-02-08 21:54:09 +0300

    Ready to production

--- src/components/stripe-button/StripeButton.jsx
+++ src/components/stripe-button/StripeButton.jsx
@@ -4,7 +4,8 @@ import StripeCheckout from 'react-stripe-checkout';
 function StripeCheckoutButton({ price }) {
   // The value of 1 dollar is 100 in Stripe
   const priceForStripe = price * 100;
-  const publishableKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
+  const publishableKey =
+    'pk_test_51KQoc4FsRdcVlUBDNAW3MfQyC6kl1gRrRT3L8fL7xdDzWcR3bxixS5GWPXn6msHF4aWCOaFqT3b1pwuMugGRGCgu0060FHy4R3';
 
   const onToken = (token) => {
     console.log(token);
